113fb5ba398c23367e8c5525cf25c44e1b7525f2
kainat999 <kainatbajwah999@gmail.com>
2023-11-04 05:18:48 +0000

    setup of chat app

--- video_11to19/app/consumers.py
+++ video_11to19/app/consumers.py
@@ -1,50 +1,73 @@
-import asyncio
-from time import sleep
 from channels.consumer import AsyncConsumer, SyncConsumer
 from channels.exceptions import StopConsumer
-import json
+from asgiref.sync import async_to_sync
 
-# SyncConsumer testing
+
+
 class TestSyncConsumer(SyncConsumer):
     def websocket_connect(self, event):
-        print('Websocket is Connected', event)
+        print('websocket Connected now ...', event)
+        # get defult channels layer fron a project
+        print("Channels Layer...", self.channel_layer)
+        # get defult channels name fron a project
+        print("Channels Name...", self.channel_name)
+        # syncConsumer cant work like async so we can import async_tosync method for this.
+        # add channel to a new or existing group.....
+        async_to_sync(self.channel_layer.group_add)(
+            'Friends',    #friends is a group name
+            self.channel_name
+            )
         self.send({
-            'type': 'websocket.accept',
+            'type': 'websocket.accept'
         })
 
+
     def websocket_receive(self, event):
-        print('Message is received....',event['text'])
-        for i in range(10):
-            self.send({
-                'type':'websocket.send',
-                'text': json.dumps({"count": i})
-            })
-            sleep(i)
+        print('Message receive from clint...', event['text'])
+        print('Type of Message receive from clint...', type(event['text']))
+        async_to_sync(self.channel_layer.group_send)('Friends', {
+            'type': 'chat.message',
+            'message': event['text']
+        })
+
+    def chat_message(self, event):
+        print('Event..', event)  
+        print('Event data...', event['message'])  
+        print('Type of Event data...', type(event['message']))  
+
+        self.send({
+            'type': 'websocket.send',
+            'text': event['message']
+        })
 
     def websocket_disconnect(self, event):
-        print('Websocket is Disconnect..... ', event)
+        print('websocket DisConnected ...', event) 
+        # get defult channels layer fron a project
+        print("Channels Layer...", self.channel_layer)
+        # get defult channels name fron a project
+        print("Channels Name...", self.channel_name)
+
+        #discard a group
+        async_to_sync(self.channel_layer.group_discard)(
+            'Friends', 
+            self.channel_name
+            )
         raise StopConsumer        
 
 
-# AsyncConsumer testing
 
 class TestAsyncConsumer(AsyncConsumer):
-
     def websocket_connect(self, event):
-        print('Websocket is Connected', event)
+        print('websocket Connected now ...', event)
         self.send({
-            'type': 'websocket.accept',
+            'type': 'websocket.accept'
         })
 
+
     def websocket_receive(self, event):
-        print('Message is received....',event['text'])
-        for i in range(10):
-            self.send({
-                'type':'websocket.send',
-                'text': json.dumps({"count": i})
-            })
-            sleep(i)
+        print('Message receive from clint...', event)
+
 
     def websocket_disconnect(self, event):
-        print('Websocket is Disconnect..... ', event)
-        raise StopConsumer        
+        print('websocket DisConnected ...', event)  
+        raise StopConsumer       
