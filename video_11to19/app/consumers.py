caec2c3f895740e18fbf02d346afcad5d6ade96f
kainat999 <kainatbajwah999@gmail.com>
2023-11-07 11:28:45 +0000

    aysnc method

--- video_11to19/app/consumers.py
+++ video_11to19/app/consumers.py
@@ -57,17 +57,52 @@ class TestSyncConsumer(SyncConsumer):
 
 
 class TestAsyncConsumer(AsyncConsumer):
-    def websocket_connect(self, event):
+  async  def websocket_connect(self, event):
         print('websocket Connected now ...', event)
+        # get defult channels layer fron a project
+        print("Channels Layer...", self.channel_layer)
+        # get defult channels name fron a project
+        print("Channels Name...", self.channel_name)
+        # syncConsumer cant work like async so we can import async_tosync method for this.
+        # add channel to a new or existing group.....
+        await self.channel_layer.group_add(
+            'Friends',    #friends is a group name
+            self.channel_name
+            )
         self.send({
             'type': 'websocket.accept'
         })
 
 
-    def websocket_receive(self, event):
-        print('Message receive from clint...', event)
+async def websocket_receive(self, event):
+        print('Message receive from clint...', event['text'])
+        print('Type of Message receive from clint...', type(event['text']))
+        await  self.channel_layer.group_send('Friends', {
+            'type': 'chat.message',
+            'message': event['text']
+        })
 
+async  def chat_message(self, event):
+        print('Event..', event)  
+        print('Event data...', event['message'])  
+        print('Type of Event data...', type(event['message']))  
 
-    def websocket_disconnect(self, event):
-        print('websocket DisConnected ...', event)  
-        raise StopConsumer       
+        await self.send({
+            'type': 'websocket.send',
+            'text': event['message']
+        })
+
+async  def websocket_disconnect(self, event):
+        print('websocket DisConnected ...', event) 
+        # get defult channels layer fron a project
+        print("Channels Layer...", self.channel_layer)
+        # get defult channels name fron a project
+        print("Channels Name...", self.channel_name)
+
+        #discard a group
+        await self.channel_layer.group_discard(
+            'Friends', 
+            self.channel_name
+            )
+        raise StopConsumer        
+    
