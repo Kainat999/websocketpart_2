bf753126e73bc12081c39a0a08653ee4d5395a28
kainat999 <kainatbajwah999@gmail.com>
2023-11-09 06:25:11 +0000

    frontent

--- GenericConsumer/app/consumers.py
+++ GenericConsumer/app/consumers.py
@@ -3,8 +3,8 @@
 
 
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
-
-
+from time import sleep  # for receive meassages in slow speed
+import asyncio # use for asyncwebsocketconsumer in sleep method
 
 
 class TestWebsocketConsumer(WebsocketConsumer):
@@ -62,6 +62,50 @@ class TestAsyncWebsocketConsumer(AsyncWebsocketConsumer):
 # This handler is called when either connection to the client is 
 # lost, either from the client closing the connection, the server 
 # closing the connection, or loss of the socket.
+
+    async def disconnect(self, close_code):
+        print('Websocket is Disconnected....', close_code)  
+
+
+
+
+# Real-time Data Example
+# Real-time Data Example with Front End
+
+
+class MyWebsocketConsumer(WebsocketConsumer):
+
+
+    def connect(self):
+        print('Websocket is connected....')
+        self.accept() 
+
+    def receive(self, text_data=None, bytes_data=None):
+        print('Message is received from client now ....', text_data)
+        for i in range(20):
+            self.send(text_data=str(i)) # To send Data to client..
+            sleep(1)
+        
+        self.close()           
+        self.close(code=4123)  
+
+    def disconnect(self, close_code):
+        print('Websocket is Disconnected....', close_code)         
+
+class MyAsyncWebsocketConsumer(AsyncWebsocketConsumer):
+
+
+    async def connect(self):
+        print('Websocket is connected....')
+        await self.accept() 
+
+    async def receive(self, text_data=None, bytes_data=None):
+        print('Message is received from client now ....', text_data)
+
+        for i in range(20):
+            await self.send(text_data=str(i)) # To send Data to client..
+            await asyncio.sleep(1)    # To send Data to Client 
+        
 
     async def disconnect(self, close_code):
         print('Websocket is Disconnected....', close_code)  
--- GenericConsumer/app/routing.py
+++ GenericConsumer/app/routing.py
@@ -4,4 +4,9 @@ from . import consumers
 websocket_urlpatterns = [
     path('ws/wsct/', consumers.TestWebsocketConsumer.as_asgi()),
     path('ws/awsct/', consumers.TestAsyncWebsocketConsumer.as_asgi()),
+
+
+    # Real time Example path
+    path('ws/wscte/', consumers.MyWebsocketConsumer.as_asgi()),
+    path('ws/awscte/', consumers.MyAsyncWebsocketConsumer.as_asgi()),
 ]
